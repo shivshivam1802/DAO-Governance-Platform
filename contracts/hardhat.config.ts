5f60f2abbcc6e13fa30cfefffc426683ae491ccf
DecentraBuilder <builder@decentradao.org>
2026-07-01 16:36:51 +0530

    chore: initialize Hardhat configuration files

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      evmVersion: "cancun",
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+  },
+};
+
+export default config;
